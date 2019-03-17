c24bcb059a8e7c16d5297089cbed8a484f43bf50
pco2699 <aenkun@gmail.com>
2019-03-17 18:23:40 +0900

    img2grayscale変更

--- img2grayscale.sh
+++ img2grayscale.sh
@@ -1,6 +1,6 @@
 
 for dir in `find articles/images_src/* -type d`; do
-    dist=${dir//images_src\//images\//}
+    dist=${dir//images_src\//images/}
     rm -rf $dist
     mkdir $dist
     for file in `find ${dir} -type f`; do
