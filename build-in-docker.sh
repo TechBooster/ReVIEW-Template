976f48270fe2586637a09c834ac26c9b726ae945
Kenshi Muto <kmuto@kmuto.jp>
2023-07-05 22:37:36 +0900

    support Re:VIEW 5.8.0

--- build-in-docker.sh
+++ build-in-docker.sh
@@ -2,6 +2,6 @@
 [ ! -z $REVIEW_CONFIG_FILE ] || REVIEW_CONFIG_FILE=config.yml
 
 # コマンド手打ちで作業したい時は以下の通り /book に pwd がマウントされます
-# docker run -i -t -v $(pwd):/book vvakame/review:5.7 /bin/bash
+# docker run -i -t -v $(pwd):/book vvakame/review:5.8 /bin/bash
 
-docker run -t --rm -v $(pwd):/book vvakame/review:5.7 /bin/bash -ci "cd /book && ./setup.sh && REVIEW_CONFIG_FILE=$REVIEW_CONFIG_FILE npm run pdf"
+docker run -t --rm -v $(pwd):/book vvakame/review:5.8 /bin/bash -ci "cd /book && ./setup.sh && REVIEW_CONFIG_FILE=$REVIEW_CONFIG_FILE npm run pdf"
